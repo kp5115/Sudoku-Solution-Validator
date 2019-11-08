78c0d95974125d1e185bf521349df42adef6b5d7
Ronak Shah <rony@Ronaks-MacBook-Pro.local>
2019-11-07 23:06:54 -0800

    added main cpp file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku-Solution-Validator
+//
+//  Created by Ronak Shah on 11/7/19.
+//  Copyright © 2019 Ronak Shah. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Test";
+    return 0;
+}
